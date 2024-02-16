b50ead990d034c35f53ec5362786bbe044188c30
Madalena Bastakou <madalena.bastakou@gmail.com>
2024-02-16 14:07:27 +0200

    add pre installation

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ import cors from "cors";
 import connectToDb from "./config/connectToDb.js";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -15,6 +18,12 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, 'frontent', 'dist', 'index.html'))
+})
 
 app.listen(2180, () => {
   console.log("Server is running on localhost:2180");
