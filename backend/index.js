e62d7eb0f8f63a77150296e030a9de9edf602014
Madalena Bastakou <madalena.bastakou@gmail.com>
2024-02-27 19:17:55 +0200

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/api/my-bookings", bookingRoutes);
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-app.length("*", (req,res) => {
+app.get("*", (req,res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
